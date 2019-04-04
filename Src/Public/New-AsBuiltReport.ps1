b666e86411467af691506d3cc94e8f466d4a9a8a
Tim Carman <tpcarman@gmail.com>
2019-04-04 22:16:35 +1100

    Update New-AsBuiltReport.ps1

--- Src/Public/New-AsBuiltReport.ps1
+++ Src/Public/New-AsBuiltReport.ps1
@@ -92,7 +92,7 @@ function New-AsBuiltReport {
             HelpMessage = 'Please specify which report type you wish to run.'
         )]
         [ValidateScript( {
-                $InstalledReportModules = Get-Module -Name "AsBuiltReport.*" #-ListAvailable
+                $InstalledReportModules = Get-Module -Name "AsBuiltReport.*" -ListAvailable
                 $ValidReports = foreach ($InstalledReportModule in $InstalledReportModules) {
                     $NameArray = $InstalledReportModule.Name.Split('.')
                     "$($NameArray[-2]).$($NameArray[-1])"
